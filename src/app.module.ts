921313d96ec264bd80a2202f0ff930a8a3990ff7
Eric Rodrigues <er601432@gmail.com>
2021-12-23 22:39:30 -0300

    feat: init configuration typeorm and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app/controller/app.controller';
 import { AppService } from './app/service/app.service';
-import { CatsModule } from './modules/cats/cats.module';
-import { OwnerModule } from './modules/owner/owner.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nestcatsowners', {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
     }),
-    CatsModule,
-    OwnerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
