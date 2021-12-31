2ea0773b675fb1e73e3c722766c155805a591c2a
Eric Rodrigues <er601432@gmail.com>
2021-12-31 13:44:13 -0300

    fix: change module config sendgrid key

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SendGridModule } from '@ntegral/nestjs-sendgrid';
 import UserController from './infra/http/user.controller';
 import User from './infra/typeorm/entities/User';
 import BCryptHash from './providers/Hash/implementations/BCryptHash';
@@ -9,13 +8,7 @@ import CreateUserService from './services/createUser.service';
 import UpdateUserService from './services/updateUser.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forFeature([User]),
-    SendGridModule.forRoot({
-      apiKey: process.env.SENDGRID_API_KEY,
-    }),
-  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [CreateUserService, UpdateUserService, BCryptHash],
 })
