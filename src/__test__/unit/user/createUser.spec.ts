3e0369aafa7678321c91f36ae0b37b41ad06939c
Eric Rodrigues <er601432@gmail.com>
2022-02-19 22:46:16 -0300

    test: concluding testing in create user service

--- src/__test__/unit/user/createUser.spec.ts
+++ src/__test__/unit/user/createUser.spec.ts
@@ -148,4 +148,50 @@ describe('Testing the functions of create users', () => {
     expect(mailerMockup.execute).toHaveBeenCalledTimes(0);
     expect(hashPasswordMockup.generateHash).toHaveBeenCalledTimes(0);
   });
+
+  it('Should not be able create user with username already in usage for other user', async () => {
+    const userCreateInputMock: Omit<User, 'id' | 'created_at' | 'updated_at'> =
+      {
+        name: 'User Test 2',
+        username: 'usertest',
+        email: 'usertest2@gmail.com',
+        password: 'qwe123',
+      };
+
+    const userCreatedOutputMock: User = {
+      id: '64282815-b4eb-4a2b-a2f4-0c2689e380a4',
+      name: 'User Test',
+      username: 'usertest',
+      email: 'usertest@gmail.com',
+      password: '$2a$12$ef9HJafpDSQ13XnxrpuU.Og9O43rbuOnUlFMn6MAU3M2qa0DsQQYi',
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    userRepositoryMockup.findOne.mockReturnValueOnce(Promise.resolve());
+    userRepositoryMockup.findOne.mockReturnValueOnce(
+      Promise.resolve(userCreatedOutputMock),
+    );
+
+    expect(
+      createUserService.execute({
+        name: userCreateInputMock.name,
+        username: userCreateInputMock.username,
+        email: userCreateInputMock.email,
+        password: userCreateInputMock.password,
+      }),
+    ).rejects.toEqual(
+      new ConflictException('This username is in usage for other user'),
+    );
+    expect(userRepositoryMockup.findOne).toHaveBeenCalled();
+    expect(userRepositoryMockup.create).not.toHaveBeenCalled();
+    expect(userRepositoryMockup.save).not.toHaveBeenCalled();
+    expect(mailerMockup.execute).not.toHaveBeenCalled();
+    expect(hashPasswordMockup.generateHash).not.toHaveBeenCalled();
+    expect(userRepositoryMockup.findOne).toHaveBeenCalledTimes(1);
+    expect(userRepositoryMockup.create).toHaveBeenCalledTimes(0);
+    expect(userRepositoryMockup.save).toHaveBeenCalledTimes(0);
+    expect(mailerMockup.execute).toHaveBeenCalledTimes(0);
+    expect(hashPasswordMockup.generateHash).toHaveBeenCalledTimes(0);
+  });
 });
